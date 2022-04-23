26b1a01c77896a6faf65168707aa20a891de9498
BaeSoHyun <qpyu66@naver.com>
2022-04-23 14:51:15 +0900

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Community
+## port 8082
---
+++ src/test/java/com/bithumb/msacommunity/MsaCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bithumb.msacommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
