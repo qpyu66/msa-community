4f5ab87ad71361cf7886b88a48e96285f1be1aa1
Bae <qpyu66@naver.com>
2022-04-24 01:23:47 +0900

    change community to board, add replyhandler

--- src/main/java/com/bithumb/msacommunity/handler/CommunityHandler.java
+++ src/main/java/com/bithumb/msacommunity/handler/CommunityHandler.java
@@ -1,21 +1,25 @@
 package com.bithumb.msacommunity.handler;
 
+import com.bithumb.msacommunity.domain.Community;
 import com.bithumb.msacommunity.service.CommunityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class CommunityHandler {
 
-    @Autowired
-    CommunityService communityService;
+//    @Autowired
+    private final CommunityService communityService;
 
     // 게시판 열람
     public Mono<ServerResponse> viewNoticeBoardHandler(ServerRequest request) {
@@ -27,13 +31,15 @@ public class CommunityHandler {
     }
 
     // 게시글 작성
-    public Mono<ServerResponse> writeCommunity(ServerRequest request) {
-        Map<String, Object> result = new HashMap<>();
-        //lectureService... 서비스로직
+    public Mono<ServerResponse> writeBoard(ServerRequest request) {
+        Mono<Community> community = request.bodyToMono(Community.class)
+                .flatMap(board -> communityService.saveBoard(board));
+
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(result, Map.class);
-    }
+                .body(community, Map.class);
+   }
+
 
     // 댓글 작성
     public Mono<ServerResponse> writeCommentHandler(ServerRequest request) {
