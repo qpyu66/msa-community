e282dca67355793a7bf21e8ef425c7a4c279ebf8
Bae <qpyu66@naver.com>
2022-04-24 14:06:26 +0900

    delete community

--- src/main/java/com/bithumb/msacommunity/repository/CommunityRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.bithumb.msacommunity.repository;
-//
-//import com.bithumb.msacommunity.domain.Community;
-//import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public interface CommunityRepository extends ReactiveCrudRepository<Community, Integer> {
-//}
