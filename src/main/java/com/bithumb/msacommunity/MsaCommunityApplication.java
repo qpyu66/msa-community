a31a4118899d548a791f2b3b8b44a5c5b4e24964
Bae <qpyu66@naver.com>
2022-04-26 22:52:57 +0900

    connect eureka

--- src/main/java/com/bithumb/msacommunity/MsaCommunityApplication.java
+++ src/main/java/com/bithumb/msacommunity/MsaCommunityApplication.java
@@ -2,9 +2,11 @@ package com.bithumb.msacommunity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsaCommunityApplication {
 
     public static void main(String[] args) {
