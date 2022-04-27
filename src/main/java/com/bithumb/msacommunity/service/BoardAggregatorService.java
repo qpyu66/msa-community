4c42e955f8896cbc66ff079102af0babe6dd1f89
seunghoon_jeong <seunghoon_jeong@tmax.co.kr>
2022-04-27 18:24:45 +0900

    주석 추가

--- src/main/java/com/bithumb/msacommunity/service/BoardAggregatorService.java
+++ src/main/java/com/bithumb/msacommunity/service/BoardAggregatorService.java
@@ -15,14 +15,27 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+/**
+ * 게시물 내용과 댓글 리스트를 결합하여 응답하는 서비스
+ */
 public class BoardAggregatorService {
     private final BoardClient boardClient;
     private final ReplyClient replyClient;
 
+    /**
+     * 게시물 내용을 가져오는 메소드
+     * @param boardId 게시물 id
+     * @return Mono.zip을 사용하여 게시물 내용과 댓글 리스트를 async & non-block 하게 결합
+     */
     public Mono<BoardAggregate> getBoard(Integer boardId) {
         return Mono.zip(this.boardClient.getBoard(boardId), this.replyClient.getReplyList(boardId)).map(this::combine);
     }
 
+    /**
+     * 튜플 개체에서 response를 결합
+     * @param tuple
+     * @return
+     */
     private BoardAggregate combine(Tuple2<Board, List<Reply>> tuple) {
         return BoardAggregate.create(tuple.getT1(), tuple.getT2());
     }
